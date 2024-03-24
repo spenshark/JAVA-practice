db429275e840ab121f35454089c4cb70725ce1a1
spenshark <xzx01299@naver.com>
2024-03-25 01:14:53 +0900

    Update NullMain1 test

--- src/basic/practice/ref/NullMain1.java
+++ src/basic/practice/ref/NullMain1.java
@@ -5,10 +5,13 @@ public class NullMain1 {
     public static void main(String[] args) {
 
         Data data = null;
-        System.out.println(data);
+        System.out.println("1   " + data);
         data = new Data();
-        System.out.println(data);
+        System.out.println("2   " + data);
         data = null;
+        System.out.println("3   " + data);
         data = new Data();
+        System.out.println("4   " + data);
+
     }
 }
