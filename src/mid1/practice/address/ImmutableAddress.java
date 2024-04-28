0b63c7b067aab23b772c61c44c86df64681c3607
spenshark <xzx01299@naver.com>
2024-04-29 00:06:58 +0900

    Inflearn Java-Mid1 Section2 commit

---
+++ src/mid1/practice/address/ImmutableAddress.java
@@ -0,0 +1,20 @@
+package mid1.practice.address;
+
+public class ImmutableAddress {
+    private final String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public ImmutableAddress(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "value='" + value + '\'' +
+                '}';
+    }
+}
---
+++ src/mid1/practice/address/RefMain2.java
@@ -0,0 +1,16 @@
+package mid1.practice.address;
+
+public class RefMain2 {
+
+    public static void main(String[] args) {
+        ImmutableAddress a = new ImmutableAddress("서울");
+        ImmutableAddress b = a;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        // b.setValue("부산");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+
+}
