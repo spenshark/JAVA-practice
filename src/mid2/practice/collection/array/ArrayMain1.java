63e579c1f7051a72720ff03b685cc9b34c098d44
spenshark <xzx01299@naver.com>
2024-05-24 17:09:29 +0900

    Inflearn Java-Mid2 Section3 commit(ArrayList)

---
+++ src/mid2/practice/collection/array/ArrayMain1.java
@@ -0,0 +1,37 @@
+package mid2.practice.collection.array;
+
+import java.util.Arrays;
+
+public class ArrayMain1 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        System.out.println("==index 입력: O(1)==");
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println("==index 입력: O(1)==");
+        arr[2] = 10;
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println("==index 조회: O(1)==");
+        System.out.println("arr[2] = " + arr[2]);
+
+        System.out.println("==배열 검색: O(n)==");
+        System.out.println(Arrays.toString(arr));
+        int value = 10;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("arr[" + i + "]: " + arr[i]);
+            if (arr[i] == value) {
+                System.out.println(value + " 찾음");
+                break;
+            }
+        }
+
+        System.out.println(arr.length);
+        System.out.println(arr[4]);
+    }
+}
---
+++ src/mid2/practice/collection/array/MyArrayListV3Main.java
@@ -0,0 +1,31 @@
+package mid2.practice.collection.array;
+
+public class MyArrayListV3Main {
+
+    public static void main(String[] args) {
+        MyArrayListV3 list = new MyArrayListV3();
+        System.out.println("==데이터 추가==");
+        System.out.println(list);
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        System.out.println(list);
+
+        System.out.println("addLast");
+        list.add(3, "addLast");
+        System.out.println(list);
+
+        System.out.println("addFirst");
+        list.add(0, "addFirst");
+        System.out.println(list);
+
+        Object removed1 = list.remove(4);
+        System.out.println("removed(4)=" + removed1);
+        System.out.println(list);
+
+        Object removed2 = list.remove(0);
+        System.out.println("removed(0)=" + removed2);
+        System.out.println(list);
+
+    }
+}
